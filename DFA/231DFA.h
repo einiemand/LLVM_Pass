f919d9ffb3463082e47ee4f4c7297d5f5e344f82
einiemand-sl <einiemand@outlook.com>
2019-03-08 15:21:20 -0800

    2nd

--- DFA/231DFA.h
+++ DFA/231DFA.h
@@ -36,33 +36,50 @@ namespace llvm {
  */
 class Info {
 public:
-    Info() {}
-    Info(const Info& other) {}
-    virtual ~Info() {};
+	Info() {}
+	Info(const Info& other) {}
+	virtual ~Info() {};
 
-    /*
-     * Print out the information
-     *
-     * Direction:
-     *   In your subclass you should implement this function according to the project specifications.
-     */
-    virtual void print() = 0;
+	/*
+	 * Print out the information
+	 *
+	 * Direction:
+	 *   In your subclass you should implement this function according to the project specifications.
+	 */
+	virtual void print() = 0;
 
-    /*
-     * Compare two pieces of information
-     *
-     * Direction:
-     *   In your subclass you need to implement this function.
-     */
-    static bool equals(Info * info1, Info * info2);
-    /*
-     * Join two pieces of information.
-     * The third parameter points to the result.
-     *
-     * Direction:
-     *   In your subclass you need to implement this function.
-     */
-    static void join(Info * info1, Info * info2, Info * result);
+	virtual const std::unordered_set<unsigned>& getEdgeInfo() const = 0;
+	virtual std::unordered_set<unsigned>& getEdgeInfo() = 0;
+
+	/*
+	 * Compare two pieces of information
+	 *
+	 * Direction:
+	 *   In your subclass you need to implement this function.
+	 */
+	static bool equals(Info* info1, Info* info2) {
+		return info1->getEdgeInfo() == info2->getEdgeInfo();
+	}
+	/*
+	 * Join two pieces of information.
+	 * The third parameter points to the result.
+	 *
+	 * Direction:
+	 *   In your subclass you need to implement this function.
+	 */
+	static void join(Info* info1, Info* info2, Info* result) {
+		if (!result) {
+			return;
+		}
+		std::unordered_set<unsigned> temp;
+		if (info1) {
+			temp.insert(info1->getEdgeInfo().cbegin(), info1->getEdgeInfo().cend());
+		}
+		if (info2) {
+			temp.insert(info2->getEdgeInfo().cbegin(), info2->getEdgeInfo().cend());
+		}
+		result->getEdgeInfo() = std::move(temp);
+	}
 
 };
 
@@ -74,257 +91,257 @@ template <class Info, bool Direction>
 class DataFlowAnalysis {
 
 protected:
-    typedef std::pair<unsigned, unsigned> Edge;
-    // Index to instruction map
-    std::map<unsigned, Instruction *> IndexToInstr;
-    // Instruction to index map
-    std::map<Instruction *, unsigned> InstrToIndex;
-    // Edge to information map
-    std::map<Edge, Info *> EdgeToInfo;
-    // The bottom of the lattice
-    Info Bottom;
-    // The initial state of the analysis
-    Info InitialState;
-    // EntryInstr points to the first instruction to be processed in the analysis
-    Instruction * EntryInstr;
+	typedef std::pair<unsigned, unsigned> Edge;
+	// Index to instruction map
+	std::map<unsigned, Instruction*> IndexToInstr;
+	// Instruction to index map
+	std::map<Instruction*, unsigned> InstrToIndex;
+	// Edge to information map
+	std::map<Edge, Info*> EdgeToInfo;
+	// The bottom of the lattice
+	Info Bottom;
+	// The initial state of the analysis
+	Info InitialState;
+	// EntryInstr points to the first instruction to be processed in the analysis
+	Instruction* EntryInstr;
 
 
-    /*
-     * Assign an index to each instruction.
-     * The results are stored in InstrToIndex and IndexToInstr.
-     * A dummy node (nullptr) is added. It has index 0. This node has only one outgoing edge to EntryInstr.
-     * The information of this edge is InitialState.
-     * Any real instruction has an index > 0.
-     *
-     * Direction:
-     *   Do *NOT* change this function.
-     *   Both forward and backward analyses must use it to assign
-     *   indices to the instructions of a function.
-     */
-    void assignIndiceToInstrs(Function * F) {
+	/*
+	 * Assign an index to each instruction.
+	 * The results are stored in InstrToIndex and IndexToInstr.
+	 * A dummy node (nullptr) is added. It has index 0. This node has only one outgoing edge to EntryInstr.
+	 * The information of this edge is InitialState.
+	 * Any real instruction has an index > 0.
+	 *
+	 * Direction:
+	 *   Do *NOT* change this function.
+	 *   Both forward and backward analyses must use it to assign
+	 *   indices to the instructions of a function.
+	 */
+	void assignIndiceToInstrs(Function* F) {
 
-        // Dummy instruction null has index 0;
-        // Any real instruction's index > 0.
-        InstrToIndex[nullptr] = 0;
-        IndexToInstr[0] = nullptr;
+		// Dummy instruction null has index 0;
+		// Any real instruction's index > 0.
+		InstrToIndex[nullptr] = 0;
+		IndexToInstr[0] = nullptr;
 
-        unsigned counter = 1;
-        for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-            Instruction * instr = &*I;
-            InstrToIndex[instr] = counter;
-            IndexToInstr[counter] = instr;
-            counter++;
-        }
+		unsigned counter = 1;
+		for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+			Instruction* instr = &*I;
+			InstrToIndex[instr] = counter;
+			IndexToInstr[counter] = instr;
+			counter++;
+		}
 
-        return;
-    }
+		return;
+	}
 
-    /*
-     * Utility function:
-     *   Get incoming edges of the instruction identified by index.
-     *   IncomingEdges stores the indices of the source instructions of the incoming edges.
-     */
-    void getIncomingEdges(unsigned index, std::vector<unsigned> * IncomingEdges) {
-        assert(IncomingEdges->size() == 0 && "IncomingEdges should be empty.");
+	/*
+	 * Utility function:
+	 *   Get incoming edges of the instruction identified by index.
+	 *   IncomingEdges stores the indices of the source instructions of the incoming edges.
+	 */
+	void getIncomingEdges(unsigned index, std::vector<unsigned>* IncomingEdges) {
+		assert(IncomingEdges->size() == 0 && "IncomingEdges should be empty.");
 
-        for (auto const &it : EdgeToInfo) {
-            if (it.first.second == index)
-                IncomingEdges->push_back(it.first.first);
-        }
+		for (auto const& it : EdgeToInfo) {
+			if (it.first.second == index)
+				IncomingEdges->push_back(it.first.first);
+		}
 
-        return;
-    }
+		return;
+	}
 
-    /*
-     * Utility function:
-     *   Get incoming edges of the instruction identified by index.
-     *   OutgoingEdges stores the indices of the destination instructions of the outgoing edges.
-     */
-    void getOutgoingEdges(unsigned index, std::vector<unsigned> * OutgoingEdges) {
-        assert(OutgoingEdges->size() == 0 && "OutgoingEdges should be empty.");
+	/*
+	 * Utility function:
+	 *   Get incoming edges of the instruction identified by index.
+	 *   OutgoingEdges stores the indices of the destination instructions of the outgoing edges.
+	 */
+	void getOutgoingEdges(unsigned index, std::vector<unsigned> * OutgoingEdges) {
+		assert(OutgoingEdges->size() == 0 && "OutgoingEdges should be empty.");
 
-        for (auto const &it : EdgeToInfo) {
-            if (it.first.first == index)
-                OutgoingEdges->push_back(it.first.second);
-        }
+		for (auto const& it : EdgeToInfo) {
+			if (it.first.first == index)
+				OutgoingEdges->push_back(it.first.second);
+		}
 
-        return;
-    }
+		return;
+	}
 
-    /*
-     * Utility function:
-     *   Insert an edge to EdgeToInfo.
-     *   The default initial value for each edge is bottom.
-     */
-    void addEdge(Instruction * src, Instruction * dst, Info * content) {
-        Edge edge = std::make_pair(InstrToIndex[src], InstrToIndex[dst]);
-        if (EdgeToInfo.count(edge) == 0)
-            EdgeToInfo[edge] = content;
-        return;
-    }
+	/*
+	 * Utility function:
+	 *   Insert an edge to EdgeToInfo.
+	 *   The default initial value for each edge is bottom.
+	 */
+	void addEdge(Instruction * src, Instruction * dst, Info * content) {
+		Edge edge = std::make_pair(InstrToIndex[src], InstrToIndex[dst]);
+		if (EdgeToInfo.count(edge) == 0)
+			EdgeToInfo[edge] = content;
+		return;
+	}
 
-    /*
-     * Initialize EdgeToInfo and EntryInstr for a forward analysis.
-     */
-    void initializeForwardMap(Function * func) {
-        assignIndiceToInstrs(func);
+	/*
+	 * Initialize EdgeToInfo and EntryInstr for a forward analysis.
+	 */
+	void initializeForwardMap(Function * func) {
+		assignIndiceToInstrs(func);
 
-        for (Function::iterator bi = func->begin(), e = func->end(); bi != e; ++bi) {
-            BasicBlock * block = &*bi;
+		for (Function::iterator bi = func->begin(), e = func->end(); bi != e; ++bi) {
+			BasicBlock* block = &*bi;
 
-            Instruction * firstInstr = &(block->front());
+			Instruction* firstInstr = &(block->front());
 
-            // Initialize incoming edges to the basic block
-            for (auto pi = pred_begin(block), pe = pred_end(block); pi != pe; ++pi) {
-                BasicBlock * prev = *pi;
-                Instruction * src = (Instruction *)prev->getTerminator();
-                Instruction * dst = firstInstr;
-                addEdge(src, dst, &Bottom);
-            }
+			// Initialize incoming edges to the basic block
+			for (auto pi = pred_begin(block), pe = pred_end(block); pi != pe; ++pi) {
+				BasicBlock* prev = *pi;
+				Instruction* src = (Instruction*)prev->getTerminator();
+				Instruction* dst = firstInstr;
+				addEdge(src, dst, &Bottom);
+			}
 
-            // If there is at least one phi node, add an edge from the first phi node
-            // to the first non-phi node instruction in the basic block.
-            if (isa<PHINode>(firstInstr)) {
-                addEdge(firstInstr, block->getFirstNonPHI(), &Bottom);
-            }
+			// If there is at least one phi node, add an edge from the first phi node
+			// to the first non-phi node instruction in the basic block.
+			if (isa<PHINode>(firstInstr)) {
+				addEdge(firstInstr, block->getFirstNonPHI(), &Bottom);
+			}
 
-            // Initialize edges within the basic block
-            for (auto ii = block->begin(), ie = block->end(); ii != ie; ++ii) {
-                Instruction * instr = &*ii;
-                if (isa<PHINode>(instr))
-                    continue;
-                if (instr == (Instruction *)block->getTerminator())
-                    break;
-                Instruction * next = instr->getNextNode();
-                addEdge(instr, next, &Bottom);
-            }
+			// Initialize edges within the basic block
+			for (auto ii = block->begin(), ie = block->end(); ii != ie; ++ii) {
+				Instruction* instr = &*ii;
+				if (isa<PHINode>(instr))
+					continue;
+				if (instr == (Instruction*)block->getTerminator())
+					break;
+				Instruction * next = instr->getNextNode();
+				addEdge(instr, next, &Bottom);
+			}
 
-            // Initialize outgoing edges of the basic block
-            Instruction * term = (Instruction *)block->getTerminator();
-            for (auto si = succ_begin(block), se = succ_end(block); si != se; ++si) {
-                BasicBlock * succ = *si;
-                Instruction * next = &(succ->front());
-                addEdge(term, next, &Bottom);
-            }
+			// Initialize outgoing edges of the basic block
+			Instruction* term = (Instruction*)block->getTerminator();
+			for (auto si = succ_begin(block), se = succ_end(block); si != se; ++si) {
+				BasicBlock* succ = *si;
+				Instruction* next = &(succ->front());
+				addEdge(term, next, &Bottom);
+			}
 
-        }
+		}
 
-        EntryInstr = (Instruction *) &((func->front()).front());
-        addEdge(nullptr, EntryInstr, &InitialState);
+		EntryInstr = (Instruction*) & ((func->front()).front());
+		addEdge(nullptr, EntryInstr, &InitialState);
 
-        return;
-    }
+		return;
+	}
 
-    /*
-     * Direction:
-     *   Implement the following function in part 3 for backward analyses
-     */
-    void initializeBackwardMap(Function * func) {
+	/*
+	 * Direction:
+	 *   Implement the following function in part 3 for backward analyses
+	 */
+	void initializeBackwardMap(Function * func) {
 
-    }
+	}
 
-    /*
-     * The flow function.
-     *   Instruction I: the IR instruction to be processed.
-     *   std::vector<unsigned> & IncomingEdges: the vector of the indices of the source instructions of the incoming edges.
-     *   std::vector<unsigned> & IncomingEdges: the vector of indices of the source instructions of the outgoing edges.
-     *   std::vector<Info *> & Infos: the vector of the newly computed information for each outgoing eages.
-     *
-     * Direction:
-     * 	 Implement this function in subclasses.
-     */
-    virtual void flowfunction(Instruction * I,
-                              std::vector<unsigned> & IncomingEdges,
-                              std::vector<unsigned> & OutgoingEdges,
-                              std::vector<Info *> & Infos) = 0;
+	/*
+	 * The flow function.
+	 *   Instruction I: the IR instruction to be processed.
+	 *   std::vector<unsigned> & IncomingEdges: the vector of the indices of the source instructions of the incoming edges.
+	 *   std::vector<unsigned> & IncomingEdges: the vector of indices of the source instructions of the outgoing edges.
+	 *   std::vector<Info *> & Infos: the vector of the newly computed information for each outgoing eages.
+	 *
+	 * Direction:
+	 * 	 Implement this function in subclasses.
+	 */
+	virtual void flowfunction(Instruction * I,
+							  std::vector<unsigned> & IncomingEdges,
+							  std::vector<unsigned> & OutgoingEdges,
+							  std::vector<Info*> & Infos) = 0;
 
 public:
-    DataFlowAnalysis(Info & bottom, Info & initialState) :
-        Bottom(bottom), InitialState(initialState), EntryInstr(nullptr) {}
+	DataFlowAnalysis(Info & bottom, Info & initialState) :
+		Bottom(bottom), InitialState(initialState), EntryInstr(nullptr) {}
 
-    virtual ~DataFlowAnalysis() = default;
+	virtual ~DataFlowAnalysis() = default;
 
-    /*
-     * Print out the analysis results.
-     *
-     * Direction:
-     * 	 Do not change this funciton.
-     * 	 The autograder will check the output of this function.
-     */
-    void print() {
-        for (auto const &it : EdgeToInfo) {
-            errs() << "Edge " << it.first.first << "->" "Edge " << it.first.second << ":";
-            (it.second)->print();
-        }
-    }
+	/*
+	 * Print out the analysis results.
+	 *
+	 * Direction:
+	 * 	 Do not change this funciton.
+	 * 	 The autograder will check the output of this function.
+	 */
+	void print() {
+		for (auto const& it : EdgeToInfo) {
+			errs() << "Edge " << it.first.first << "->" "Edge " << it.first.second << ":";
+			(it.second)->print();
+		}
+	}
 
-    /*
-     * This function implements the work list algorithm in the following steps:
-     * (1) Initialize info of each edge to bottom
-     * (2) Initialize the worklist
-     * (3) Compute until the worklist is empty
-     *
-     * Direction:
-     *   Implement the rest of the function.
-     *   You may not change anything before "// (2) Initialize the worklist".
-     */
-    void runWorklistAlgorithm(Function * func) {
-        std::deque<unsigned> worklist;
+	/*
+	 * This function implements the work list algorithm in the following steps:
+	 * (1) Initialize info of each edge to bottom
+	 * (2) Initialize the worklist
+	 * (3) Compute until the worklist is empty
+	 *
+	 * Direction:
+	 *   Implement the rest of the function.
+	 *   You may not change anything before "// (2) Initialize the worklist".
+	 */
+	void runWorklistAlgorithm(Function * func) {
+		std::deque<unsigned> worklist;
 
-        // (1) Initialize info of each edge to bottom
-        if (Direction)
-            initializeForwardMap(func);
-        else
-            initializeBackwardMap(func);
+		// (1) Initialize info of each edge to bottom
+		if (Direction)
+			initializeForwardMap(func);
+		else
+			initializeBackwardMap(func);
 
-        assert(EntryInstr != nullptr && "Entry instruction is null.");
+		assert(EntryInstr != nullptr && "Entry instruction is null.");
 
-        // (2) Initialize the work list
-        std::unordered_set<unsigned> inList;
+		// (2) Initialize the work list
+		std::unordered_set<unsigned> inList;
 
-        for (auto& edgeInfo : EdgeToInfo) {
-            unsigned node1 = edgeInfo.first.first, node2 = edgeInfo.first.second;
-            if (!inList.count(node1)) {
-                worklist.push_back(node1);
-                inList.insert(node1);
-            }
-            if (!inList.count(node2)) {
-                worklist.push_back(node2);
-                inList.insert(node2);
-            }
-        }
+		for (auto& edgeInfo : EdgeToInfo) {
+			unsigned node1 = edgeInfo.first.first, node2 = edgeInfo.first.second;
+			if (!inList.count(node1)) {
+				worklist.push_back(node1);
+				inList.insert(node1);
+			}
+			if (!inList.count(node2)) {
+				worklist.push_back(node2);
+				inList.insert(node2);
+			}
+		}
 
-        // (3) Compute until the work list is empty
-        while (!worklist.empty()) {
-            unsigned curNode = worklist.front();
-            worklist.pop_front();
-            inList.erase(curNode);
+		// (3) Compute until the work list is empty
+		while (!worklist.empty()) {
+			unsigned curNode = worklist.front();
+			worklist.pop_front();
+			inList.erase(curNode);
 
-            Instruction* curInstr = IndexToInstr[curNode];
+			Instruction* curInstr = IndexToInstr[curNode];
 
-            std::vector<unsigned> incomingEdges, outgoingEdges;
-            getIncomingEdges(curNode, &incomingEdges);
-            getOutgoingEdges(curNode, &outgoingEdges);
+			std::vector<unsigned> incomingEdges, outgoingEdges;
+			getIncomingEdges(curNode, &incomingEdges);
+			getOutgoingEdges(curNode, &outgoingEdges);
 
-            std::vector<Info*> newOutInfos;
-            flowfunction(curInstr, incomingEdges, outgoingEdges, newOutInfos);
+			std::vector<Info*> newOutInfos;
+			flowfunction(curInstr, incomingEdges, outgoingEdges, newOutInfos);
 
-            for (std::size_t i = 0; i < newOutInfos.size(); ++i) {
-                unsigned nexNode = outgoingEdges[i];
-                Edge outEdge = std::make_pair(curNode, nexNode);
-                Info* oldOutInfo = EdgeToInfo[outEdge];
-                Info* newOutInfo = newOutInfos[i];
-                if (!Info::equals(oldOutInfo, newOutInfo)) {
-                    EdgeToInfo[outEdge] = newOutInfo;
-                    if (!inList.count(nexNode)) {
-                        worklist.push_back(nexNode);
-                        inList.insert(nexNode);
-                    }
-                }
-            }
-        }
-        print();
-    }
+			for (std::size_t i = 0; i < newOutInfos.size(); ++i) {
+				unsigned nexNode = outgoingEdges[i];
+				Edge outEdge = std::make_pair(curNode, nexNode);
+				Info* oldOutInfo = EdgeToInfo[outEdge];
+				Info* newOutInfo = newOutInfos[i];
+				if (!Info::equals(oldOutInfo, newOutInfo)) {
+					EdgeToInfo[outEdge] = newOutInfo;
+					if (!inList.count(nexNode)) {
+						worklist.push_back(nexNode);
+						inList.insert(nexNode);
+					}
+				}
+			}
+		}
+		print();
+	}
 };
 
 
