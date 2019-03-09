e80ec4fac26db8da296207dff9117b37f1dfebcc
einiemand-sl <einiemand@outlook.com>
2019-03-08 18:41:10 -0800

    8th

--- DFA_pt3/LivenessAnalysis.cpp
+++ DFA_pt3/LivenessAnalysis.cpp
@@ -41,54 +41,54 @@ class LivenessInfo : public Info {
 private:
 	std::unordered_set<unsigned> insts;
 };
-//
-//class LivenessAnalysis : public DataFlowAnalysis<LivenessInfo, false> {
-//public:
-//	LivenessAnalysis(LivenessInfo& bottom, LivenessInfo& initialState) :
-//		DataFlowAnalysis<LivenessInfo, false>(bottom, initialState)
-//	{
-//	}
-//
-//	virtual ~LivenessAnalysis() override = default;
-//private:
-//	static const std::unordered_set<unsigned> opCodes;
-//
-//	virtual void flowfunction(Instruction* I,
-//							  std::vector<unsigned>& IncomingEdges,
-//							  std::vector<unsigned>& OutgoingEdges,
-//							  std::vector<LivenessInfo*>& Infos) override
-//	{
-//		if (!I) {
-//			return;
-//		}
-//		unsigned curIndex = InstrToIndex[I];
-//		LivenessInfo outInfo;
-//		for (unsigned preIndex : IncomingEdges) {
-//			Edge incomingEdge = std::make_pair(preIndex, curIndex);
-//			LivenessInfo::join(EdgeToInfo[incomingEdge], &outInfo, &outInfo);
-//		}
-//
-//		if (!isa<PHINode>(I)) {
-//			if (I->isBinaryOp() ||
-//				I->isShift() ||
-//				I->isBitwiseLogicOp() ||
-//				opCodes.count(I->getOpcode())) 
-//			{
-//					
-//			}
-//		}
-//	}
-//
-//};
-//
-//const std::unordered_set<unsigned> LivenessAnalysis::opCodes{
-//	Instruction::Alloca,
-//	Instruction::Load,
-//	Instruction::GetElementPtr,
-//	Instruction::ICmp,
-//	Instruction::FCmp,
-//	Instruction::PHI,
-//	Instruction::Select
-//};
+
+class LivenessAnalysis : public DataFlowAnalysis<LivenessInfo, false> {
+public:
+	LivenessAnalysis(LivenessInfo& bottom, LivenessInfo& initialState) :
+		DataFlowAnalysis<LivenessInfo, false>(bottom, initialState)
+	{
+	}
+
+	virtual ~LivenessAnalysis() override = default;
+private:
+	static const std::unordered_set<unsigned> opCodes;
+
+	virtual void flowfunction(Instruction* I,
+							  std::vector<unsigned>& IncomingEdges,
+							  std::vector<unsigned>& OutgoingEdges,
+							  std::vector<LivenessInfo*>& Infos) override
+	{
+		if (!I) {
+			return;
+		}
+		unsigned curIndex = InstrToIndex[I];
+		LivenessInfo outInfo;
+		for (unsigned preIndex : IncomingEdges) {
+			Edge incomingEdge = std::make_pair(preIndex, curIndex);
+			LivenessInfo::join(EdgeToInfo[incomingEdge], &outInfo, &outInfo);
+		}
+
+		if (!isa<PHINode>(I)) {
+			if (I->isBinaryOp() ||
+				I->isShift() ||
+				I->isBitwiseLogicOp() ||
+				opCodes.count(I->getOpcode())) 
+			{
+					
+			}
+		}
+	}
+
+};
+
+const std::unordered_set<unsigned> LivenessAnalysis::opCodes{
+	Instruction::Alloca,
+	Instruction::Load,
+	Instruction::GetElementPtr,
+	Instruction::ICmp,
+	Instruction::FCmp,
+	Instruction::PHI,
+	Instruction::Select
+};
 
 }
